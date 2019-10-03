6c2efe29f829c3223e4405b2728da646546cd7eb
Bart Hunerbein <Bart.Hunerbein@intecbrussel.be>
2019-10-03 15:52:03 +0200

    finished HigherLower game

--- src/be/intecbrussel/programming_language/exercises/HigherLower.java
+++ src/be/intecbrussel/programming_language/exercises/HigherLower.java
@@ -1,8 +1,34 @@
 package be.intecbrussel.programming_language.exercises;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HigherLower {
 
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        Random randomNumberGenerator = new Random();
+        int numberToGuess = randomNumberGenerator.nextInt(100);
+        System.out.println("enter a number:");
+        boolean guessed = false;
+        while(!guessed){
+            int guess = keyboard.nextInt();
+            if(guess<numberToGuess){
+                System.out.println("higher!");
+            }
+            else if( guess>numberToGuess){
+                System.out.println("lower!");
+            }
+            else{
+                System.out.println("you got it!");
+                guessed = true;
+            }
+
+
+
+        }
+
 
     }
 
