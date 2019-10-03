ea993638900908207c6925e7886b259b621de476
Bart Hunerbein <Bart.Hunerbein@intecbrussel.be>
2019-10-03 15:15:43 +0200

    added WhileApp.java

---
+++ src/be/intecbrussel/programming_language/demo/WhileApp.java
@@ -0,0 +1,16 @@
+package be.intecbrussel.programming_language.demo;
+
+public class WhileApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("counting to ten");
+        int i =0;
+        while (i<=10){
+            System.out.println(++i);
+        }
+
+
+    }
+
+}
