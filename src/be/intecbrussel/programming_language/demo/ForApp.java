8e9d0e44f4ffb4fd05c56e7c6da988539b82e907
Bart Hunerbein <Bart.Hunerbein@intecbrussel.be>
2019-10-03 15:15:31 +0200

    added ForApp.java

---
+++ src/be/intecbrussel/programming_language/demo/ForApp.java
@@ -0,0 +1,26 @@
+package be.intecbrussel.programming_language.demo;
+
+public class ForApp {
+
+    public static void main(String[] args) {
+        System.out.println("take a bowl");
+        System.out.println("add flour");
+        System.out.println("add milk");
+        System.out.println("add eggs");
+        System.out.println("add vanilla sugar");
+
+        for(int iteration = 0; iteration<10; iteration++){
+            System.out.println("whisk the batter");
+        }
+
+        System.out.println("pour the batter in the pan");
+        System.out.println("put the pan on the fire");
+        System.out.println("wait till the batter is dry on top");
+        System.out.println("throw pancake into the air like you dont care");
+        System.out.println("catch");
+        System.out.println("wait 34 sec");
+        System.out.println("serve pancake");
+
+    }
+
+}
